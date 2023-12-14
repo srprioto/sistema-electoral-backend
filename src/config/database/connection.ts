96dde3e3a6d9d35d7e965f8b00828d40b576a3ee
prioto <hailprioto@gmail.com>
2023-12-13 21:29:33 -0500

    correcciones upload

--- src/config/database/connection.ts
+++ src/config/database/connection.ts
@@ -1,16 +1,16 @@
 import { Sequelize } from 'sequelize';
 
+// const namedb:string = "u533990259_sistema_electo";
+// const userdb:string = "u533990259_sis_electo";
+// const passdb:string = "TjHSC5#2?y2/";
+// const hostdb:string = "srv794.hstgr.io";
+
 const namedb:string = "sistema_electoral";
 const userdb:string = "root";
 const passdb:string = "";
-const hostdb:string = "localhost"
-const dialectdb:any = "mysql"
+const hostdb:string = "localhost";
 
-// const namedb = process.env.NAMEDB || "";
-// const userdb = process.env.USERDB || "";
-// const passdb = process.env.PASSDB || "";
-// const hostdb = process.env.HOSTDB || "";
-// const dialectdb:any = process.env.DIALECTDB;
+const dialectdb:any = "mysql";
 
 
 export const db = new Sequelize(
@@ -36,3 +36,11 @@ const dbConnection = async () => {
 
 
 export default dbConnection;
+
+
+
+
+// const namedb = process.env.NAMEDB || "sistema_electoral";
+// const userdb = process.env.USERDB || "root";
+// const passdb = process.env.PASSDB || "";
+// const hostdb = process.env.HOSTDB || "localhost";
